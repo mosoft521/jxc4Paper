e0298c5ae70029890ad911b2311d51272388261d
Alvin Zhang <mosoft521@gmail.com>
2020-05-12 16:07:42 +0800

    init

---
+++ src/main/java/com/gmail/mosoft521/Jxc4PaperApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.mosoft521;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jxc4PaperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jxc4PaperApplication.class, args);
+    }
+
+}
